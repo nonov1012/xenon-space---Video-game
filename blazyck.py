a38696981957ec7f29d2f03ccf6d7214d958eae6
nonov1012 <noa.voiturier@gmail.com>
2025-09-19 15:02:25 +0200

    feat: image des planetes

---
+++ blazyck.py
@@ -0,0 +1,9 @@
+# Constantes
+
+# Taille de la map
+NB_CASE_X = 50 # Nombre de cases en largeur
+NB_CASE_Y = 30 # Nombre de cases en hauteur
+TAILLE_CASE = 35 # Taille d'une case en pixels
+
+# Chemain des fichiers
+PATH_IMG_PLANETES = "assets/img/planets/"
