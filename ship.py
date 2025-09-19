a09a976ce98e780c189c4e34b5369f154b0dd8c3
reclea <clementnoel62@gmail.com>
2025-09-19 11:13:48 +0200

    feat: menu

--- ship.py
+++ ship.py
@@ -1,38 +1,60 @@
-# programme crée par Dupuis Brian
-# Ce programme continent la class ship,
+# Programme créé par Dupuis Brian
+# Ce programme contient la classe Ship
 
 import pygame
+import numpy as np
 
-# class ship permet la création des vaisseaux et gére toute les choses en rapport avec les mécanique du vaisseau
+# La classe Ship permet la création des vaisseaux et gère toutes les mécaniques associées
 class Ship:
-    def __init__(self, PV_max: int, att :int, distance_att : int, distance_dep : int, cout : int , money_dead : int, taille, minage : bool, transport : bool, img : pygame.image, tiers : int):
-        self.PV_max = PV_max    # PV max du vaisseau
-        self.PV_actuelle = PV_max   # PV actuelle du Vaisseau
-        self.att = att  # dégâts du vaisseau
-        self.distance_att = distance_att    # distance d'attaque du vaisseau
-        self.distance_dep = distance_dep    # distance de deplacement du vaisseau
-        self.cout = cout    # cout du vaisseau
-        self.money_dead = money_dead    # l'argent que rapporte ce navire a l'adversaire
-        self.taille = taille    # taille du vaisseau sur la carte
-        self.minage = minage    # peut t'il miner ? 
-        self.transport = transport  # peut-il transporte d'autre vaisseau
-        self.stockage = None    # ce qu'il contient
-        self.img = img          # continent le lien de l'image du vaisseau
-        self.tiers = tiers      # le tier de la base requie pour le crée
+    def __init__(self, PV_max: int, att: int, distance_att: int, distance_dep: int,
+                 cout: int, money_dead: int, taille, minage: bool, transport: bool, 
+                 img: pygame.image, tiers: int):
+        
 
-    """def ma_methode(self):
-        # Exemple de méthode
-        print("Méthode appelée")
+        self.PV_max = PV_max              # Points de vie maximum du vaisseau
+        self.PV_actuelle = PV_max         # Points de vie actuels du vaisseau
+        self.att = att                    # Dégâts du vaisseau
+        self.distance_att = distance_att  # Portée d'attaque du vaisseau
+        self.distance_dep = distance_dep  # Distance de déplacement du vaisseau
+        self.cout = cout                  # Coût de construction du vaisseau
+        self.money_dead = money_dead      # L'argent rapporté à l'adversaire en cas de destruction
+        self.taille = taille              # Taille du vaisseau sur la carte
+        self.minage = minage              # Peut-il miner ?
+        self.transport = transport        # Peut-il transporter d'autres vaisseaux ?
+        stockage = np.array([None, None, None], dtype=Ship)
+        self.stockage = stockage              # Contenu du vaisseau (si applicable)
+        self.img = img                    # Image du vaisseau
+        self.tiers = tiers                # Niveau requis pour le construire
+        self.cordonner_x = 1
+        self.cordonner_y = 1
+        self.direction = 2
 
-    def __str__(self):
-        return f"MaClasse(param1={self.param1}, param2={self.param2})"""
+    def rotate(self, map):  # Méthode qui gère le déplacement
+        pass
 
-img = pygame.image.load("img.png")
-petit = Ship(200, 75, 3, 3, 325, (325*0.6), 1, False, False, img, 1)    #expemple de creation d'un vaisseau petit
-moyen = Ship(400, 175, 4, 4, 650, (650*0.6), 1, False, False, img, 1)   #expemple de creation d'un vaisseau moyen
-foreuse = Ship(300, 0, 0, 3, 400, (400*0.6), 1, False, False, img, 2)   #expemple de creation d'une foreuse
-transporteur = Ship(600, 60, 6, 6, 500, (500*0.6), 2, False, False, img, 3) #expemple de creation d'un transporteur
-grand = Ship(750, 300, 6, 6, 1050, (1050*0.6), 2, False, False, img, 4) #expemple de creation d'un vaisseau grand
+    def move(self, type_case):  # Méthode qui gère le déplacement
+        pass
+
+    def attack(self, ship_defendu):  # Méthode qui gère l'attaque
+        ship_defendu.take_damage(self.att)
+
+    def draw(self, map):  # ?
+        pass
 
 
+    def take_damage(self, ship_attaquant):  # Méthode qui gère la prise de dégâts
+        self.PV_actuelle = self.PV_actuelle - ship_attaquant
+
+    def dead(self):  # Méthode qui gère la destruction du vaisseau
+        pass
+
+
+# Exemple de création de vaisseaux
+img = pygame.image.load("test.png")
+
+petit = Ship(200, 75, 3, 3, 325, (325 * 0.6), 1, False, False, img, 1)       # Vaisseau petit
+moyen = Ship(400, 175, 4, 4, 650, (650 * 0.6), 2, False, False, img, 1)      # Vaisseau moyen
+foreuse = Ship(300, 0, 0, 3, 400, (400 * 0.6), 1, True, False, img, 2)       # Foreuse
+#transporteur = Ship(600, 60, 6, 6, 500, (500 * 0.6), 2, False, True, img, 3) # Transporteur
+#grand = Ship(750, 300, 6, 6, 1050, (1050 * 0.6), 2, False, False, img, 4)    # Grand vaisseau
 
