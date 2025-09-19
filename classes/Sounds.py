ca6326b1a993faef1bf82024ce44f794360efaeb
Ugo <119074028+GitLovox@users.noreply.github.com>
2025-09-19 14:09:03 +0200

    feat: ajout des volumes

--- classes/Sounds.py
+++ classes/Sounds.py
@@ -7,6 +7,8 @@ class SoundManager:
         self.music_channel = pygame.mixer.Channel(0)
         self.sfx_channel = pygame.mixer.Channel(1)
         self.sounds = {}
+        self.music_volume = 100
+        self.sfx_volume = 100
     
     # --- Gestion musique ---
     def play_music(self, filepath, loops=-1):
@@ -16,6 +18,14 @@ class SoundManager:
 
     def stop_music(self):
         self.music_channel.stop()
+        
+    def set_music_volume(self, volume):
+        """
+        Définit le volume de la musique.
+        volume: int entre 0 et 100
+        """
+        self.music_volume = volume
+        self.music_channel.set_volume(volume / 100)
         
     # --- Gestion SFX ---
     def load_sfx(self, name, filepath):
@@ -29,6 +39,17 @@ class SoundManager:
 
     def stop_sfx(self):
         self.sfx_channel.stop()
+        
+    def set_sfx_volume(self, volume):
+        """
+        Définit le volume des effets sonores.
+        volume: int entre 0 et 100
+        """
+        self.sfx_volume = volume
+        # applique volume à tous les SFX chargés
+        for sound in self.sounds.values():
+            sound.set_volume(volume / 100)
+        self.sfx_channel.set_volume(volume / 100)
 
 
 if __name__ == "__main__":
@@ -37,5 +58,8 @@ if __name__ == "__main__":
     sm.play_music("assets/sounds/musics/music_ingame.mp3")
     sm.load_sfx("laser_base", "assets/sounds/base/laser_base.mp3")
     sm.play_sfx("laser_base")
+    pygame.time.wait(3000)
+    print("volume = 10 %")
+    sm.set_music_volume(10)
     
     pygame.time.wait(30000000)
