8ec82186c69aa7119df0ca8398b29c18f38ce57b
NockXu <gabriel.dav@outlook.fr>
2025-10-03 00:28:23 +0200

    Feat(Mecanique de tour)

--- classes/Turn.py
+++ classes/Turn.py
@@ -1,5 +1,5 @@
-from __future__ import annotations
 from typing import List
+from classes.Ship import Ship
 from classes.Player import Player
 
 class Turn:
@@ -7,12 +7,11 @@ class Turn:
     sentence = "Tour de"
 
     @classmethod
-    def play(cls):
+    def next(cls):
         if not cls.players:
             print("Aucun joueur")
             return
         player = cls.players.pop(0)
-        player.play()
         cls.players.append(player)
 
     @classmethod
@@ -20,9 +19,17 @@ class Turn:
         if not cls.players:
             return "Aucun joueur"
         return f"{cls.sentence} {cls.players[0].name}"
+    
+    @classmethod
+    def get_players_ships(cls) -> list:
+        ships_list = []
+        for player in cls.players:
+            ships_list.extend(player.ships)
+        return ships_list
 
 if __name__ == "__main__":
     Turn.players = [Player("Alice"), Player("Bob")]
     print(Turn.describe())
-    Turn.play()
+    Turn.next()
     print(Turn.describe())
+    print(Turn.get_players_ships())
