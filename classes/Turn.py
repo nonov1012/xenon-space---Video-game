32ca2104ccd10df010ab1c19e0b1b304465e9863
NockXu <gabriel.dav@outlook.fr>
2025-10-04 14:25:56 +0200

    Ajout nb tours joué

--- classes/Turn.py
+++ classes/Turn.py
@@ -4,7 +4,8 @@ from classes.Player import Player
 
 class Turn:
     players : list[Player] = []
-    sentence = "Tour de"
+    sentence = "Tour"
+    _nb_turns : float = 1
 
     @classmethod
     def next(cls):
@@ -13,12 +14,15 @@ class Turn:
             return
         player = cls.players.pop(0)
         cls.players.append(player)
+        cls._nb_turns += 1 / len(cls.players)
+        
 
     @classmethod
     def describe(cls):
+        res : str = f"{cls.sentence} {str(cls.get_nb_turns())} :"
         if not cls.players:
             return "Aucun joueur"
-        return f"{cls.sentence} {cls.players[0].name}"
+        return f"{res} {cls.players[0].name}"
     
     @classmethod
     def get_players_ships(cls) -> list:
@@ -26,9 +30,15 @@ class Turn:
         for player in cls.players:
             ships_list.extend(player.ships)
         return ships_list
+    
+    @classmethod
+    def get_nb_turns(cls) -> int:
+        return int(cls._nb_turns)
 
 if __name__ == "__main__":
     Turn.players = [Player("Alice"), Player("Bob")]
+    print(Turn.describe())
+    Turn.next()
     print(Turn.describe())
     Turn.next()
     print(Turn.describe())
